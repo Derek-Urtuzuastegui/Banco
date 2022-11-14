26ed051259fda0d3e2a17b4f6ad746a113a305d3
Derek <manelikk.securytec@gmail.com>
2022-11-13 23:36:07 -0600

    final nip

--- mali/main.js
+++ mali/main.js
@@ -11,7 +11,7 @@ function depositar(){
     var saldoMaximo =('Una dsiculpa, solo es posible tener $990 en la cuenta por lo que $' + olaNumero + ' no fue posible ingresarlos a su cuenta');
     if(olaNumero>0){
        NIP = parseInt(prompt('porfavor ingresse su NIP para seguir con la operacion'))
-       if(NIP == '1234'){
+       if(NIP == '3214'){
         cantidad+=olaNumero
        }else{
         alert('Lo siento su nip es incorrecto, intentelo nuevamente')
@@ -33,7 +33,7 @@ function retirar(){
     var olaNumero = parseFloat(cantidadIngresar)
     if(olaNumero>0 && cantidad<990){
         NIP = parseInt(prompt('porfavor ingresse su NIP para seguir con la operacion'))
-       if(NIP == '1234'){
+       if(NIP == '3214'){
         cantidad-=olaNumero
        }else{
         alert('Lo siento su nip es incorrecto, intentelo nuevamente')
